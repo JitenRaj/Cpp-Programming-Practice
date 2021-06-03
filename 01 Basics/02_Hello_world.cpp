afa89b7b4896caac57e09682816fb452de45748b
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-03 23:31:13 +0530

    Hello world

---
+++ 01 Basics/02_Hello_world.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main() {
+  std::cout<<"Hello world";
+  /* as we didn't include std in namespace so here cout
+  is undefined. To make it valid we have to use it as 
+
+  std::cout 
+
+  std stand for standard and 
+  cout stand for console out
+  */
+  return 0;
+}
