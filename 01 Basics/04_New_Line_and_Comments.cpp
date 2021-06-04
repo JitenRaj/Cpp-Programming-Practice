448f7ae36d93aaa89daaed3eed6cc767be780cba
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-04 23:24:41 +0530

    New Line and Comment

---
+++ 01 Basics/04_New_Line_and_Comments.cpp
@@ -0,0 +1,12 @@
+/* This is 
+muliple line
+comment*/
+
+#include<iostream>
+using namespace std;
+int main() {
+  cout<<"Hello\n";
+  // this is single line comment
+  cout<<"World";
+  return 0;
+}
