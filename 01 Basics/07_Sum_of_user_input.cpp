e1d5fca7c2f33872e5e42b2bf0e915b5c5dcf46f
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-04 23:38:58 +0530

    addition of user inputs

---
+++ 01 Basics/07_Sum_of_user_input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int num1,num2,sum;
+  cout<<"Enter First Number: ";
+  cin>> num1;
+  cout<<"Enter Second Number: ";
+  cin>> num2;
+
+  sum = num1 + num2;
+  cout<<"Sum of two number is: " << sum;
+  return 0;
+}
