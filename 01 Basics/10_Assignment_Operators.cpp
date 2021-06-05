9a8e8181bc199e96e832bab1d81ba91cce0b8ef3
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-05 23:40:17 +0530

    Assignment Operators

---
+++ 01 Basics/10_Assignment_Operators.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int num1=10;
+  cout <<"Number is : " << num1 << endl;
+
+  //Addition
+  // num1 = num1 + 8
+  num1 += 8;
+  cout<<num1<<endl;
+
+  //Subtraction
+  // num1 = num1 - 6
+  num1 -= 6;
+  cout << num1 << endl;
+
+  // Multiplication
+  // num1 = num1 * 4
+  num1 *= 4;
+  cout << num1 <<endl;
+
+  // Division
+  // num1 = num1 / 2
+  num1 /= 2;
+  cout << num1 <<endl;
+
+  // Modulus
+  // num1 = num1 % 10
+  num1 %= 10;
+  cout << num1 << endl;
+
+  return 0;
+}
