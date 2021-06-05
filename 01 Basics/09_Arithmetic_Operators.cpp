3da189300b8ceabb8c9bf4457a11a1bf6684abb8
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-05 23:18:42 +0530

    Arithmetic Operators

---
+++ 01 Basics/09_Arithmetic_Operators.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+  // Variables num1 and num2
+  int num1=12,num2=4;
+  cout<<"num1 = "<<num1<<endl<<"num2 = "<<num2<<endl;
+
+  // Addition
+  int sum = num1 + num2;
+  cout<<"num1 + num2 = "<<sum<<endl;
+  // Subtraction
+  int sub = num1 - num2;
+  cout<<"num1 - num2 = "<<sub<<endl;
+  // Multipliction
+  int mul = num1*num2;
+  cout<<"num1 * num2 = "<<mul<<endl;
+  // Division
+  int div = num1/num2;
+  cout<<"num1 / num2 = "<<div<<endl;
+  // Modulus (Remainder)
+  int rem = num1%num2;
+  cout<<"num1 % num2 = "<<rem<<endl;
+}
