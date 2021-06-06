606563669537e75164727ffa0b9e8232e50ce8f8
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-06 13:11:54 +0530

    Relational Operator

---
+++ 02 Conditions/02_relational_Operator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+  if (10==10) // True
+  {
+    cout<<"10 is equal to 10"<<endl;
+  }
+  if(4>=2) // True
+  {
+    cout<<"4 is greater than or equal to 2"<<endl;
+  }
+  if(5<=6) // True
+  {
+    cout<<"5 is less than or equal to 6"<<endl;
+  }
+  if(4!=6) // True
+  {
+    cout<<"4 is not equal to 6"<<endl;
+  }
+  return 0;
+}
