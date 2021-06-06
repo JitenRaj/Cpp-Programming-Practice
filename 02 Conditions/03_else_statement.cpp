c6d0d54233b3ad3777ba68a82f141ff765cbaeff
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-06 13:52:21 +0530

    Else statement

---
+++ 02 Conditions/03_else_statement.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int x ;
+  cout<<"Enter your age: ";
+  cin>> x ;
+  if(x>=18){
+    cout<<"Welcome";
+  }
+  else {
+    cout<<"You are nder age";
+  }
+  return 0;
+}
