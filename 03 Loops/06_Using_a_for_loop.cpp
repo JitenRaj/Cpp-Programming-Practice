e2118b456bceaf00caa38188d938e81f3167036a
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-11 00:05:22 +0530

    using for loop

--- 03 Loops/06_Using_a_for_loop.cpp
+++ 03 Loops/06_Using_a_for_loop.cpp
@@ -3,7 +3,11 @@
 #include<iostream>
 using namespace std;
 int main() {
-  int i,j,sum=0;
+  int i,j;
+  int sum=0; // initializing sum = 0
+
+/* This for loop will keep incrementing i's value and 
+adding into sum, until loop terminate */
 
   for(i = 0 ; i < 10 ; i++ ) {
     sum += i;
@@ -11,11 +15,14 @@ int main() {
     cout<<"sum is = "<<sum<<endl;
   }
 
-  int remain = sum;
-  for(j = i -1; j > 0 ; j-- ) {
-    remain -= j;
+/* This for loop will keep decrementing j's value and 
+subtracting from sub, until loop terminate */
+
+  int sub = sum; // initializing sub's value equal to sum's value
+  for(j = i -1; j > 0 ; j-- ) { // j = i-1 , to avoid negative sub, beacuse i = 10 here 
+    sub -= j;
     cout<<"j is = "<< j <<endl;
-    cout<<"remain is = "<< remain <<endl;
+    cout<<"sub is = "<< sub <<endl;
   }
   return 0;
 }
