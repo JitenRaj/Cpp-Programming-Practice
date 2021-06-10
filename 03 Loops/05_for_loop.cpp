3e9f1e8b721003fc143300030dd97359007657c0
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-10 23:42:57 +0530

    for loop

---
+++ 03 Loops/05_for_loop.cpp
@@ -0,0 +1,17 @@
+/* For loop
+Increment / Decrement */
+
+#include<iostream>
+using namespace std;
+int main() {
+  // Increament
+  for(int i = 0 ; i < 10 ; i++ ) {
+    cout<<"For Loop Working : "<< i <<endl;
+  }
+
+  // Decrement
+  for(int i = 10 ; i > 0 ; i-- ) {
+    cout<<"For Loop Working : "<< i <<endl;
+  }
+  return 0;
+}
