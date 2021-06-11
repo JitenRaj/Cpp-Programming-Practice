bfb21209d504dfbf5e7165ec61799b7d3df67a10
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-11 22:59:01 +0530

    while loop

---
+++ 03 Loops/07_do_while.cpp
@@ -0,0 +1,13 @@
+/* do while loop*/
+
+#include<iostream>
+using namespace std;
+int main() {
+  int num=0;
+  do
+  {
+    cout<<"Do while loop : "<<num<<endl;
+    num++;
+  } while (num<10);
+  
+}
