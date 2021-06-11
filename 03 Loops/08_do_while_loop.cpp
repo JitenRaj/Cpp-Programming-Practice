8542ffc0ff367a90b70415b2d5bc25f4fa99883e
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-11 22:59:54 +0530

    do while loop

---
+++ 03 Loops/08_do_while_loop.cpp
@@ -0,0 +1,15 @@
+/* do while loop excuted atleast once,
+even if certain condition is false*/
+
+#include<iostream>
+using namespace std;
+int main() {
+  int num=20;
+  int limit = 10;
+  do
+  {
+    cout<< limit << " is greater than " << num <<endl;
+    num++;
+  } while (num<limit);
+  
+}
