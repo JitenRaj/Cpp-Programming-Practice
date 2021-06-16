6bbfcfd94c82a380e11adb5b6695ef08cf321ede
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-06-16 23:09:02 +0530

    Float and Double Data Types

---
+++ 04 Data Types/05_Float_Double_Data_Type.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float f = 5.2;
+    double d = 5.59695;
+
+    cout << " Float : " << f << endl;
+    cout << " Double : " << d << endl;
+
+    return 0;
+}
