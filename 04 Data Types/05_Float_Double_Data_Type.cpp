d8f939df111cf297ac467dfe507da87821fa9d00
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-06-17 22:57:45 +0530

    Float and Double

--- 04 Data Types/05_Float_Double_Data_Type.cpp
+++ 04 Data Types/05_Float_Double_Data_Type.cpp
@@ -4,11 +4,22 @@ using namespace std;
 
 int main()
 {
-    float f = 5.2;
+    float f = 5.2f;
     double d = 5.59695;
 
     cout << " Float : " << f << endl;
     cout << " Double : " << d << endl;
+
+   // Creating a double type variable
+    double a = 3.912348239293;
+
+    // Creating a float type variable
+    float b = 3.912348239293f;
+
+    // Printing the two variables
+    cout << "Double Type Number  = " << a << endl;
+    cout << "Float Type Number  = " << b << endl;
+
 
     return 0;
 }
