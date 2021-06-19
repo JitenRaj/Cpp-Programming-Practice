1af1aa3629a5f902f9f6a22d12ca833f735178fc
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-06-19 21:54:32 +0530

    Absolute number

--- 04 Data Types/04_abosulte_value.cpp
+++ 04 Data Types/04_abosulte_value.cpp
@@ -2,20 +2,18 @@
 using namespace std;
 
 int main() {
-     
-     int a = -42;
-     int abs_a;     
-   cout << "chapter Integer - Examples of integers would include 42, -42, and similar numbers\n";
+   int a = -42;
+   int abs_a;     
 
-      cout << "Finds the absolute value of an integer.\n";     
-  
-     if(a >= 0)     
-        abs_a = a;     
-     else         
-        abs_a = -a;      
-        
-     cout << "The absolute value of " << a ;
-     cout << " is " << abs_a << endl;     
-     
-    return 0;
+   cout << "Finds the absolute value of an integer.\n";     
+   
+   if(a >= 0)     
+      abs_a = a;     
+   else         
+      abs_a = -a;      
+   
+   cout << "The absolute value of " << a ;
+   cout << " is " << abs_a << endl;     
+   
+   return 0;
 }
