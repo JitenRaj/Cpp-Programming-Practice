631f166c6ea055a469e823789bc6ff3c28c60664
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-05 23:00:19 +0530

    unsigned int

---
+++ 04 Data Types/08_unsigned_int.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    unsigned long int a;
+    cout<<"enter a number: ";
+    cin >> a ;
+    cout<< "the number is " << a <<endl;
+    return 0;
+}
