168fea2f572226c57c322aaff1c24127ce7724f1
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-05 23:10:57 +0530

    array initializing

---
+++ 05 Arrays/01_arrays_initializing.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[5];
+    int b[5] = {10,20,30,40,50};
+    cout<< a[1] <<endl;
+    cout<< b[0] <<endl;
+    cout<< b[1] <<endl;
+    cout<< b[2] <<endl;
+    cout<< b[3] <<endl;
+    cout<< b[4] <<endl;
+    cout<< b[5] <<endl;
+    return 0;
+}
