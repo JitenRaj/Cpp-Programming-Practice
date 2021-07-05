702cd5ca8ea0b7edb6414bc5515a31d4c10adc58
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-05 22:53:10 +0530

    boolean data type

---
+++ 04 Data Types/07_bool_Data_Type.cpp
@@ -0,0 +1,14 @@
+/* boolean data type
+ true = 1
+ false = 0 */
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool online = false;
+    bool logged_in = true;
+
+    cout<<online<<endl;
+    cout<<logged_in<<endl;
+    return 0;
+}
