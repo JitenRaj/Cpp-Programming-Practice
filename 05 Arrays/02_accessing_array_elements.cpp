d5bf16a8de5d1536180603deda67453dde6e7b50
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-05 23:16:03 +0530

    accessing array element

---
+++ 05 Arrays/02_accessing_array_elements.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num[5] = {10,20,30,40,50};
+    cout<< num[2] <<endl;
+
+    num[2] = 60;
+    cout<< num[2] <<endl;
+
+    return 0;
+}
